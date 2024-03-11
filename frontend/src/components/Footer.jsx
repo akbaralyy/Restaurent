02b0e21f67d5d47edc2c2fc88a4a4c97ab79810b
akbaraly <akbaralya7n@gmail.co>
2024-03-11 14:47:24 +0530

    first commit

---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,28 @@
+// eslint-disable-next-line no-unused-vars
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer>
+      <div className="container">
+        <div className="banner">
+          <div className="left">akbar</div>
+          <div className="right">
+            <p>K-1409 KW shristi rajnagar extension ghaziabad up India</p>
+            <p>Open: 05:00 PM - 12:00 AM</p>
+          </div>
+        </div>
+        <div className="banner">
+          <div className="left">
+            <p>Developed By Akbar</p>
+          </div>
+          <div className="right">
+            <p>All Rights Reserved By Akbar Ali.</p>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
