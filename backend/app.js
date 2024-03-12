ffcfbd5f13bb02e8b9fa3fd06ca4a795f596e351
akbaraly <akbaralya7n@gmail.co>
2024-03-13 00:48:00 +0530

    4

--- backend/app.js
+++ backend/app.js
@@ -8,8 +8,22 @@ import { dbConnection } from "./database/dbConnection.js";
 const app = express();
 dotenv.config({ path: "./config/config.env" });
 
-app.use(cors()); // Enable CORS for all routes
-app.options("*", cors()); // Enable pre-flight request handling
+const allowedOrigins = [process.env.FRONTEND_URL]; // Update with your frontend URL
+
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      // Check if the request origin is in the allowedOrigins array
+      if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+    methods: ["POST"],
+    credentials: true,
+  })
+);
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
