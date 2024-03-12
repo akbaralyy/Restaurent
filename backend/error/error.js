aeac3055b7113e34400d638516e4e1db2a17bf63
akbaraly <akbaralya7n@gmail.co>
2024-03-13 00:30:09 +0530

    last1

--- backend/error/error.js
+++ backend/error/error.js
@@ -1,13 +1,13 @@
-class ErrorHandler extends Error {
-    constructor (message, statusCode) {
-        super(message);
-        this.statusCode = statusCode;
-    }
-}
+import { ErrorHandler } from "./error.js";
 
 export const errorMiddleware = (err, req, res, next) => {
     err.message = err.message || "Internal Server Error";
     err.statusCode = err.statusCode || 500;
+
+    // Set the CORS headers to allow requests from your frontend domain
+    res.setHeader("Access-Control-Allow-Origin", process.env.FRONTEND_URL);
+    res.setHeader("Access-Control-Allow-Methods", "POST"); // Allow only POST requests
+    res.setHeader("Access-Control-Allow-Credentials", "true"); // Allow sending cookies
 
     return res.status(err.statusCode).json({
         success: false,
