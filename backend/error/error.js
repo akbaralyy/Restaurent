8afee674e574728e93e675a67627210dbf80b5e9
akbaraly <akbaralya7n@gmail.co>
2024-03-12 23:41:30 +0530

    Update files with LF line endings

--- backend/error/error.js
+++ backend/error/error.js
@@ -1,9 +1,10 @@
 class ErrorHandler extends Error {
-    constructor (massage, statusCode){
-        super(massage);
+    constructor (message, statusCode) {
+        super(message);
         this.statusCode = statusCode;
     }
 }
+
 export const errorMiddleware = (err, req, res, next) => {
     err.message = err.message || "Internal Server Error";
     err.statusCode = err.statusCode || 500;
@@ -13,4 +14,5 @@ export const errorMiddleware = (err, req, res, next) => {
         message: err.message,
     });
 };
+
 export default ErrorHandler;
