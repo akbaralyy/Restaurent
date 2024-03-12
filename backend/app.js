0c066e862c4e728aea7fcff1b6244b8fd31c6151
akbaraly <akbaralya7n@gmail.co>
2024-03-13 00:42:48 +0530

    last3

--- backend/app.js
+++ backend/app.js
@@ -4,21 +4,19 @@ import cors from "cors";
 import { errorMiddleware } from "./error/error.js";
 import reservationRouter from "./routes/reservationRoute.js";
 import { dbConnection } from "./database/dbConnection.js";
+
 const app = express();
 dotenv.config({ path: "./config/config.env" });
 
-app.use(
-    cors({
-      origin: [process.env.FRONTEND_URL],
-      methods: ["POST"],
-      credentials: true,
-    })
-  );
-  app.use(express.json());
-  app.use(express.urlencoded({ extended: true }));
+app.use(cors()); // Enable CORS for all routes
+app.options("*", cors()); // Enable pre-flight request handling
 
-  app.use("/api/v1/reservation", reservationRouter);
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
- dbConnection();
- app.use(errorMiddleware);
+app.use("/api/v1/reservation", reservationRouter);
+
+dbConnection();
+app.use(errorMiddleware);
+
 export default app;
